f297189c2d63ac0782e6c92ecbc4f5bb79be831c
Melissa Tiangtae <melissa_tiangate@cmu.ac.th>
2024-12-13 15:23:43 +0700

    lab9_1_completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,31 +3,54 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
+	int i=1;
+    double loan,rate,pay;
+    double prevbalance,interest, total, newbalance,payment;
+
+	cout << "Enter initial loan: ";
+	cin >> loan;
+	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
+	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+
+		cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
+
+
+	newbalance = loan;
+
+	do{
+		
+		prevbalance = newbalance;
+	interest = (rate/100)*prevbalance;
+	total = prevbalance + interest;
+	payment = pay;
+	newbalance = total - payment;
+
+
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << prevbalance;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	if(payment > total){
+		payment = total;
+		newbalance = 0;
+	}
+	cout << setw(13) << left << payment;
+	cout << setw(13) << left << newbalance;
+	cout << "\n";
+	i++;
+
+	}while(newbalance > 0);
+
 	
 	return 0;
 }
